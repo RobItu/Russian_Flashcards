d0409e7b12b49309226a07e77f5b45f6969db690
RobItu <74835755+RobItu@users.noreply.github.com>
2022-03-18 15:18:25 -0600

    Finished adding last functions to remove known words from list of all words and added a separate text entry explaining to users what to click.

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
-current_card = None
+current_card = {}
 # --------------------------- Reading from CSV --------------------- #
+try:
+    words_list = pandas.read_csv("words_to_learn.csv").to_dict(orient="records")
 
-words_list = pandas.read_csv("data/russian_words.csv").to_dict(orient="records")
+except FileNotFoundError:
+    words_list = pandas.read_csv("data/russian_words.csv").to_dict(orient="records")
+
 
 # -----------------------------Button Functions --------------------- #
 
-
-def update_word():
+def next_card():  # Word that has not been learnt will remain in deck until learnt
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(words_list)
@@ -23,10 +26,19 @@ def update_word():
     flip_timer = window.after(3000, switch)
 
 
+def known_word():  # Word that was learnt must be removed from deck
+
+    words_list.remove(current_card)
+    dic = pandas.DataFrame(words_list)
+    dic.to_csv('words_to_learn.csv', index=False, header=["Russian", "English"])
+    next_card()
+
+
 def switch():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(canvas_image, image=back_card_image)
+
 
 # --------------------------- GUI --------------------------------------------------- #
 
@@ -50,13 +62,16 @@ card_word = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
-
-right_button = Button(image=right_image, highlightthickness=0, command=update_word)
+right_button = Button(image=right_image, highlightthickness=0, command=known_word)
 right_button.grid(column=1, row=1)
 
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=update_word)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
-update_word()
+explanation_label = Label(text="Click 'Cross' if you DON'T know the word, 'Check' otherwise!",
+                          font=("Arial", 30, "bold"), bg=BACKGROUND_COLOR)
+explanation_label.grid(column=0,row=2, columnspan=2)
+
+next_card()
 
 window.mainloop()
